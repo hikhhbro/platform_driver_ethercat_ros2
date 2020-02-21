4d48982fc46695bca4055c2b99aefdb285396bcb
Tim Wiese <tim.wiese@esa.int>
2020-02-21 19:03:40 +0100

    Fix log output order issue and install signal handler for proper shutdown

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
+#include <csignal>
+
 #include "rclcpp/rclcpp.hpp"
 #include "PlatformDriverEthercatNode.h"
 
+std::unique_ptr<platform_driver_ethercat::PlatformDriverEthercatNode> platform_driver;
+
+void signalHandler(int signum)
+{
+    platform_driver->shutdown();
+}
+
 int main(int argc, char * argv[])
 {
+    // set stdout to unbuffered as a workaround to https://github.com/ros2/rcutils/issues/168
+    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+
+    // install signal handler to cleanly shutdown lifecycle node
+    signal(SIGINT, signalHandler);
+
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<platform_driver_ethercat::PlatformDriverEthercatNode>()->get_node_base_interface());
+    platform_driver = std::make_unique<platform_driver_ethercat::PlatformDriverEthercatNode>();
+    rclcpp::spin(platform_driver->get_node_base_interface());
+    platform_driver.reset();
     rclcpp::shutdown();
+
     return 0;
 }
